f14f3b88e8c5b79acc1d7d2bd297bec8ca4f5c3d
Craig Burke <craig@craigburke.com>
2015-06-24 12:00:05 -0400

    Cleanup

--- src/main/java/com/craigburke/JsClosureWrapProcessor.java
+++ src/main/java/com/craigburke/JsClosureWrapProcessor.java
@@ -1,13 +1,12 @@
 package com.craigburke;
 
 import asset.pipeline.AssetCompiler;
-import asset.pipeline.AssetPipelineConfigHolder;
 import asset.pipeline.AssetFile;
 
 public class JsClosureWrapProcessor {
-	
+
 	public JsClosureWrapProcessor(AssetCompiler precompiler) {}
-		
+	
 	public String process(String input, AssetFile assetFile) {
 		return "(function(){\n" + input + "\n})();";
 	}
--- src/test/groovy/com/craigburke/JsClosureWrapProcessorSpec.groovy
+++ src/test/groovy/com/craigburke/JsClosureWrapProcessorSpec.groovy
@@ -13,22 +13,22 @@ class JsClosureWrapProcessorSpec  extends Specification {
     AssetFile assetFile
 
     def setup() {
-        assetFile = new GenericAssetFile(path: 'foo/bar.js')
+		assetFile = new GenericAssetFile(path: 'foo/bar.js')
     }
 	
 	@Unroll("Wrapping JS: #input")
 	def "process HTML input"() {
-    	given:
-        def processor = new JsClosureWrapProcessor()
+		given:
+		def processor = new JsClosureWrapProcessor()
 
 		expect:
 		String result = processor.process(input, assetFile)
 		result.startsWith('(function(){')
 		result.contains(input)
 		result.endsWith'})();'
-		
-        where:
-        input << ["console.log('FOO');", "function(){ console.log('Closure within a closure? sure');}()"]
+
+		where:
+		input << ["console.log('FOO');", "function(){ console.log('Closure within a closure? sure');}()"]
 	}
-	
+
 }
